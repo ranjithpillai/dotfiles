c4c496607ab780d4116835f36bec294ac7fe2406
Jennifer Nguyen <j.cnguyen3@gmail.com>
2020-12-28 15:20:34 -0600

    Cmder: Update aliases

--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -18,8 +18,8 @@ home=cd %UserProfile%
 workspace=cd %UserProfile%\Workspace
 dotfiles=cd %UserProfile%\Workspace\Core\dotfiles\
 notes=cd %UserProfile%\Workspace\Core\development-notes
-notesc=cd %UserProfile%\Workspace\Core\development-notes\2020
 personal=cd %UserProfile%\Workspace\Personal
 q4j=cd "%UserProfile%\Workspace\Quest for Justice"
+q4jc=cd "%UserProfile%\Workspace\Quest for Justice\Code"  
 
 open=explorer
